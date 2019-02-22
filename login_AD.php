153da189426326c451d328801d0f28dd218ff09a
fatepsycho <fatepsycho@gmail.com>
2019-02-22 09:10:16 -0800

    Update login_AD.php
    - fix responsive form dan carousel

--- login_AD.php
+++ login_AD.php
@@ -1,9 +1,12 @@
 </!DOCTYPE html>
 <html>
 <head>
+	<meta name="viewport" content="width=device-width">
 	<title></title>
-<link href="framework/css/bootstrap.css" rel="stylesheet">
+<!--<link href="framework/css/bootstrap.css" rel="stylesheet">-->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 <style type="text/css">
+
 	.box {
 		padding: 20px;
 		margin-top: 170px;
@@ -28,15 +31,29 @@
 	}
 
 	form {
-		margin-top: 150px;
+		margin-top: 170px;
 	}
 
+	@media (max-width: 768px)
+	{
+		#gambar {
+			display: none;
+		}
+	}
+
+	@media (max-width: 970)
+	{
+		#gambar {
+			height: 50%;
+		}
+	}
+	
 </style>
 </head>
 <body>
 		<div class="row" style="margin: 0px;">
 		<div class="col-sm-12 col-lg-4" style="background-color: #16a085;  padding: 0; margin: 0;">
-			<img src="img/bgad2.jpg" class="img-responsive" style="opacity: 0.4; height: 100%; width: 100%;">
+			<img src="img/bgad2.jpg" class="img-responsive" id="gambar" style="opacity: 0.4; height: 100%; width: 100%;">
 		</div>
 		<div class="col-sm-12 col-lg-8">
 			<form action="operator/login_OP.php" method="POST">
@@ -64,13 +81,17 @@
 				</div>
 				<p>
 				<div class="form-group col-sm-12 col-lg-8">
-					<a href="#" class="help-block col-lg-4 col-lg-offset-6">Forgot Password</a>
-					<input type="submit" class="btn btn-primary col-lg-2 pull-right" name="admin_submit" style="background-color: #16a085; border: none; border-radius: 0px;" value="Login">
+					<a href="#" class="help-block col-sm-10 col-md-10 col-lg-4 col-lg-offset-6">Forgot Password</a>
+					<input type="submit" class="btn btn-primary col-sm-2 col-md-2 col-lg-2 pull-right" name="admin_submit" style="background-color: #16a085; border: none; border-radius: 0px;" value="Login">
 				</div>
 			</form>
 			<p>
 		</div>
 	</div>
 		<?php include 'operator/login_OP.php';?>
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+
+
 </body>
 </html>
