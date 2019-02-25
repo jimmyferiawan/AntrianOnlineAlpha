69220fd9e289695f9b9f33bbd4689fda8f269505
Bagas-Owner <INC.blackwolf@gmail.com>
2019-02-25 09:49:30 +0700

    navbar tunggal
    op, user, dan dokter 1 file navbar

--- user_antrian.php
+++ user_antrian.php
@@ -46,26 +46,7 @@
 </head>
 <body>
 
-    <nav class="navbar navbar-default navbar-fixed-top">
-        <div class="container-fluid">
-            <div class="navbar-header">
-                <a class="navbar-brand" href="">
-                    LOGO
-                </a>
-                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navheader-collapse" aria-expanded="false">
-                    <span class="glyphicon glyphicon-menu-hamburger"></span>
-                </button>
-
-            </div>
-            <div class="collapse navbar-collapse navheader-collapse">
-            <ul class="nav navbar-nav navbar-right">
-                <li class="active"><a href="">Ambil Antrian</a></li>
-                <li class="<?= $btn_antri_disabled ?>"><a href="user_editbio.php" >pengaturan</a></li>
-                <li class="<?= $btn_antri_disabled ?>"><a href="user/logout.php" >keluar</a></li>
-            </ul>
-            </div>
-        </div>
-    </nav>
+    <?php include 'navbar.php'; ?>
 
     <div class="container">
         <div class="row">
