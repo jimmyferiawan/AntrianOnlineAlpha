4a236024508b88c04203d5a3c95d263a73c72a13
Bagas-Owner <INC.blackwolf@gmail.com>
2019-02-25 14:03:43 +0700

    Update navbar.php

--- navbar.php
+++ navbar.php
@@ -27,7 +27,7 @@
         </div>
     </nav>
 <?php elseif (isset($_SESSION["id"]["id_op"])): ?>
-   				<a class="navbar-brand" href="">
+   				<a class="navbar-brand" href="op_index.php">
                     LOGO 
                 </a>
                 <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navheader-collapse" aria-expanded="false">
@@ -37,12 +37,19 @@
             </div>
             <div class="collapse navbar-collapse navheader-collapse">
             <ul class="nav navbar-nav navbar-right">
-                <li class="active"><a href="">Info Antrian</a></li>
+               
 				<?php
 				if($tingkat_op==1){
 				echo '<li><a href="op_requser.php">request operator baru</a></li>';}?>
-                <li><a href="op_editbio.php">pengaturan</a></li>               
-                <li><a href="operator/logout.php">keluar</a></li>
+				<li class="active"><a href="">Info Antrian</a></li>
+                <!-- <li><a href="op_editbio.php">pengaturan</a></li>               
+                <li><a href="operator/logout.php">keluar</a></li> -->
+                <li class="dropdown">
+		          	<a href="#" class="dropdown-toggle" style="text-transform: capitalize;" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><span class="glyphicon glyphicon-user"></span> <?php echo $_SESSION["id"]["id_op"]; ?> <span class="caret"></span></a>
+		       		<ul class="dropdown-menu">
+		            <li><a href="op_editbio.php">Profil Operator</a></li>
+		            <li><a href="operator/logout.php">Keluar</a></li>
+		            
             </ul>
             </div>
         </div>
