2fc45438d204ffa49952144d4280d9fa4007a27e
Bagas-Owner <INC.blackwolf@gmail.com>
2019-02-25 13:42:55 +0700

    update navbar user=op 1 file

--- op_index.php
+++ op_index.php
@@ -102,7 +102,7 @@ exit();
 	$_SESSION["id"]["tingkat_op"] =  $tingkat_op;
 ?>
 
-<nav class="navbar navbar-default" style="margin-bottom: 0px; ">
+<!-- <nav class="navbar navbar-default" style="margin-bottom: 0px; ">
         <div class="container-fluid">
             <div class="navbar-header">
                 <a class="navbar-brand" href="">
@@ -125,7 +125,10 @@ exit();
             </ul>
             </div>
         </div>
-    </nav>	
+    </nav>	 -->
+    <?php include 'navbar.php'; ?>
+
+    
 
 <div class="container">
 	<h1 class="text-center"><b>INFO ANTRIAN <h3><?php include 'operator/Jamserver.php'; ?></h3></h1>
