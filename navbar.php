aa0fb71a194734bbca46c603563cd1c42ee91bc4
Bagas-Owner <INC.blackwolf@gmail.com>
2019-02-25 13:00:35 +0700

    Update navbar.php

--- navbar.php
+++ navbar.php
@@ -13,12 +13,24 @@
             <ul class="nav navbar-nav navbar-right">
                 <li class="active"><a href="">Ambil Antrian</a></li>
                 <li class="<?= $btn_antri_disabled ?>"><a href="user_editbio.php" >pengaturan</a></li>
-                	<?php if (isset($_SESSION['u'])): ?>
+                	
+
+                	<!-- <?php if (isset($_SESSION['u'])): ?>
 					<li style="text-transform: capitalize;"><a href="profil.php"><span class="glyphicon glyphicon-user"></span> <?php echo $_SESSION['u']; ?></a></li>
 					
 					<?php else: ?>
-					<?php endif ?>
-                <li class="<?= $btn_antri_disabled ?>"><a href="user/logout.php" >keluar</a></li>
+					<?php endif ?> -->
+
+
+                <!-- <li class="<?= $btn_antri_disabled ?>"><a href="user/logout.php" >keluar</a></li> -->
+
+					<li class="dropdown">
+		          	<a href="#" class="dropdown-toggle" style="text-transform: capitalize;" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><span class="glyphicon glyphicon-user"></span> <?php echo $_SESSION['u']; ?> <span class="caret"></span></a>
+		       		<ul class="dropdown-menu">
+		            <li><a href="#">Profil Pengguna</a></li>
+		            <li><a href="user/logout.php">Keluar</a></li>
+		            
+
             </ul>
             </div>
         </div>
