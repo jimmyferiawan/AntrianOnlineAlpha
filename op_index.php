8da5b04098da93f55667265bc44a435af753fa01
fatepsycho <fatepsycho@gmail.com>
2019-02-26 11:28:24 -0800

    op_index.php
    - update UI

--- op_index.php
+++ op_index.php
@@ -15,6 +15,7 @@ exit();
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title></title>
 	<link href="framework/css/bootstrap.css" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
 <style type="text/css">
 	form .form-horizontal .form-group label .control-label {
     	display: inline-block;
@@ -32,6 +33,7 @@ exit();
     	background-color: white;
     	border-radius: 0px;
     	border: dashed 4px #36d7b7;
+
 	}
 
 	.btn-primary {
@@ -50,9 +52,16 @@ exit();
             color: #36d7b7;
     }
 
-        .navbar-default .navbar-nav li>a:hover {
-            color: #36d7b7;
-        }
+    .navbar-default .navbar-nav li>a:hover {
+            color: #36d7b7;  
+    }
+
+   @font-face {
+         font-family: "Roboto Thin";
+         src: url('framework/fonts/Roboto-Thin.ttf');
+         }
+
+		
 </style>
 </head>
 <body>
@@ -135,26 +144,28 @@ exit();
 <div class="container" style="margin-top: 90px;">
 	<div class="row">
 		<div class="col-lg-6">
-			<div class="jumbotron" style="border-radius: 3px; border: none; background-color: #71EEB8; margin-bottom: 1px; margin-top: 40px;">
+			<div class="jumbotron" style="border-radius: 3px; border: none; background-image: url('img/jackie-dilorenzo-1187244-unsplash.jpg'); background-size: cover; margin-bottom: 1px; margin-top: 40px; opacity: 0.8;">
 				<div class="row text-center">
 					<div class="col-sm-5">
-						<?php echo "<h1 style='font-size: 150px; font-weight: bold;'>".$now."</h1>";?>
-						<h4><i>SAAT INI</i></h4>
+						<?php echo "<h1 style='font-size: 150px; font-weight: bold; font-family: Roboto Thin; color: white;'>".$now."</h1>";?>
+						<h4 style="color: white;"><i>SAAT INI</i></h4>
 					</div>
 						<div class="col-sm-2">
-							<h1 style="font-size: 130px;">:</h1>
+							<h1 style="font-size: 100px; padding-top: 20px; color: white;">:</h1>
 						</div>
 					<div class="col-sm-5">
-						<h1 style="font-size: 150px; font-weight: bold;"><?php echo $count; ?></h1>
-						<h4><i>TOTAL</i></h4>
+						<h1 style="font-size: 150px; font-weight: bold; font-family: Roboto Thin; color: white;"><?php echo $count; ?></h1>
+						<h4 style="color: white;"><i>TOTAL</i></h4>
 					</div>
-				</div>
-			</div>
-			<form action="op_index.php" method="post">
+					<form action="op_index.php" method="post">
 			<div class="form-group">
-			<button class="btn btn-success btn-lg col-lg-12" name="next" id="next" style="border-radius: 3px; background-color: #48887B;">Lanjut</button>
+			<button class="btn btn-success btn-lg col-lg-12" name="next" id="next" style="border-radius: 0px; background-color: #48887B; border: none;">Lanjut</button>
 			</div>
 			</form>
+				</div>
+
+			</div>
+			
 		</div>
 		<div class="col-lg-6">
 			<ul class="nav nav-tabs col-sm-10" id="mytab" role="tablist">
@@ -167,37 +178,37 @@ exit();
   <div class="form-group">
 	<div class="col-sm-4">
 		<label for="nama" style="text-align: left;">PIN:</label>
-		<input type="text" class="form-control" id="onnama" >
+		<input type="text" class="form-control input-sm" id="onnama" >
 	</div>
   </div>
   <div class="form-group">
 	<div class="col-sm-10">
 		<label for="nama" style="text-align: left;">Nama:</label>
-		<input type="text" class="form-control" id="onnama" readonly>
+		<input type="text" class="form-control input-sm" id="onnama" readonly>
 	</div>
   </div>
   <div class="form-group">
     <div class="col-sm-5">
     	<label for="antrian" style="text-align: left;">No Antrian:</label>
-		<input type="text" class="form-control" id="antrian" readonly>
+		<input type="text" class="form-control input-sm" id="antrian" readonly>
 	</div>
   </div>
   <div class="form-group">
     <div class="col-sm-3">
     	<label for="jam" style="text-align: left;">Jam:</label>
-		<input type="text" class="form-control" id="jam" readonly>
+		<input type="text" class="form-control input-sm" id="jam" readonly>
 	</div>
   </div>
   <div class="form-group">
     <div class="col-sm-5">
     	<label for="tgl" style="text-align: left;">Tanggal:</label>
-		<input type="text" class="form-control" id="tgl" readonly>
+		<input type="text" class="form-control input-sm" id="tgl" readonly>
 	</div>
   </div>
   <div class="form-group">
     <div class="col-sm-12">
     	<label for="lokasi" style="text-align: left;">Lokasi:</label>
-		<input type="text" class="form-control" id="lokasi" readonly>
+		<input type="text" class="form-control input-sm" id="lokasi" readonly>
 	</div>
   </div>
   <div class="form-group">
@@ -212,13 +223,13 @@ exit();
 	<div class="form-group">
 		<div class="col-sm-12">
 			<label for="nama">Nama:</label>
-			<input type="text" class="form-control" name="offnama" id="offnama" placeholder="Nama">
+			<input type="text" class="form-control input-sm" name="offnama" id="offnama" placeholder="Nama">
 		</div>
 	</div>
 
 	<div class="form-group">
 		<div class="col-sm-10 col-lg-12 text-center">
-		<button class="btn btn-primary col-lg-12" style="background-color:  #48887B; border-radius: 0px; border: none;" name="antri" id="antri">ANTRI</button>
+		<button class="btn btn-primary col-lg-4" style="background-color:  #48887B; border-radius: 0px; border: none;" name="antri" id="antri">ANTRI</button>
 		</div>
 	</div>	
   </form>
@@ -227,6 +238,7 @@ exit();
 		</div>
 	</div>
 </div>
+
 <script src="framework/js/jquery-3.3.1.min.js"></script>
     <script src="framework/js/bootstrap.min.js"></script>
 </body>
