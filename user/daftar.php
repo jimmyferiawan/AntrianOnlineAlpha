2f374f85a4ee401664424ce4190289497b3fc07b
Bagas aji Nugroho <hematenergi@users.noreply.github.com>
2019-03-01 10:00:28 +0700

    Update daftar.php

--- user/daftar.php
+++ user/daftar.php
@@ -1,7 +1,7 @@
 <?php
     if(isset($_POST['pasien_submit'])) {
         require_once '../koneksi.php';
-
+        $status='2';
         $uname = $conn->real_escape_string($_POST['username']);
         $nama = $conn->real_escape_string($_POST['nama_lengkap']);
         $nik = $conn->real_escape_string($_POST['nik']);
@@ -11,7 +11,7 @@
         $pass = $conn->real_escape_string($_POST['password']);
         $bpjs = $conn->real_escape_string($_POST['bpjs']);
 
-        $sql = "INSERT INTO pasien (ID_pasien, username_pasien, password_pasien, nama_pasien, jenis_kelamin_pasien, alamat_pasien, no_hp_pasien, no_bpjs_pasien) VALUES ('$nik', '$uname', '$pass', '$nama', '$jenis_kelamin', '$alamat', '$no_hp', '$bpjs')";
+        $sql = "INSERT INTO pasien (ID_pasien, username_pasien, password_pasien, nama_pasien, jenis_kelamin_pasien, alamat_pasien, no_hp_pasien, no_bpjs_pasien, status_pasien ) VALUES ('$nik', '$uname', '$pass', '$nama', '$jenis_kelamin', '$alamat', '$no_hp', '$bpjs','$status')";
 
         $hasil = $conn->query($sql);
 
