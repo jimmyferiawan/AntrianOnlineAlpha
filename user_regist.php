16a9c7afd7f5340c771859cdf456a8fa1c199cae
Bagas aji Nugroho <hematenergi@users.noreply.github.com>
2019-03-12 09:06:24 +0700

    bug fix

--- user_regist.php
+++ user_regist.php
@@ -73,13 +73,24 @@
     </style>
 </head>
 <body>
-    <nav class="navbar navbar-default navbar-fixed-top">
-        <div class="container">
+   <nav class="navbar navbar-default navbar-fixed-top" style="background-color: white; box-shadow: 0px -1px 4px -1px;">
+        <div class="container-fluid">
             <div class="navbar-header">
-                <a class="navbar-brand" href="login.php">
-                    Logo
+                <a class="navbar-brand" href="user_regist.php">
+                    AntriSehat
                 </a>
-                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#form-login" aria-expanded="false">Login</button>
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navheader-collapse" aria-expanded="false">
+                    <span class="glyphicon glyphicon-menu-hamburger"></span>
+                </button>
+
+            </div>
+            <div class="collapse navbar-collapse navheader-collapse">
+            <ul class="nav navbar-nav navbar-right">
+                    <li class="dropdown">
+                    <a href="#" class="dropdown-toggle" style="text-transform: capitalize;" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"  id="aw"><span class="glyphicon glyphicon-user">
+                    <ul class="dropdown-menu">
+                    <li><a href="login.php" class="glyphicon glyphicon-log-in">  Login</a></li>           
+            </ul>
             </div>
         </div>
     </nav>
@@ -90,19 +101,19 @@
                     <form action="/AntrianOnlineAlpha/user/daftar.php" method="post">
                         <div class="form-group col-lg-6">
                             <label for="username">Username</label>
-                            <input type="text" name="username" id="username" class="form-control">
+                            <input type="text" name="username" id="username" class="form-control" required="">
                         </div>
                         <div class="form-group col-lg-6">
                             <label for="nama_lengkap">Nama Lengkap</label>
-                            <input type="text" name="nama_lengkap" id="nama-lengkap" class="form-control">
+                            <input type="text" name="nama_lengkap" id="nama-lengkap" class="form-control" required="">
                         </div>
                         <div class="form-group col-sm-12 col-md-6 col-lg-6">
                             <label for="password">Password</label>
-                            <input type="password" name="password" id="password" class="form-control">
+                            <input type="password" name="password" id="password" class="form-control" required="">
                         </div>
                         <div class="form-group col-sm-12 col-md-6 col-lg-6">
                             <label for="password_re">Konfirmasi Password</label>
-                            <input type="password" name="password_re" id="password_re" class="form-control">
+                            <input type="password" name="password_re" id="password_re" class="form-control" required="">
                         </div>
                         <div class="form-group col-sm-12 col-md-12 col-lg-12">
                             <label>Jenis Kelamin</label>
@@ -115,19 +126,19 @@
                         </div>
                         <div class="form-group col-lg-12">
                             <label for="alamat">Alamat</label>
-                            <textarea class="form-control" name="alamat" rows="4" id="alamat"></textarea>
+                            <textarea class="form-control" name="alamat" rows="4" id="alamat" required=""></textarea>
                         </div> 
                         <div class="form-group col-lg-6">
                             <label for="nik">NIK</label>
-                            <input type="text" name="nik" id="nik" class="form-control">
+                            <input type="text" name="nik" id="nik" class="form-control" required="">
                         </div>
                         <div class="form-group col-lg-6">
                             <label for="no_hp">Nomor Hp</label>
-                            <input type="text" name="no_hp" id="no_hp" class="form-control">
+                            <input type="text" name="no_hp" id="no_hp" class="form-control" required="">
                         </div>
                         <div class="form-group col-lg-12">
                             <label for="bpjs">Nomor BPJS</label>
-                            <input type="text" name="bpjs" id="bpjs" class="form-control">
+                            <input type="text" name="bpjs" id="bpjs" class="form-control" required="">
                         </div>
 
                         <div class="col-lg-12">
