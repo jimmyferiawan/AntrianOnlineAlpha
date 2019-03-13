204df7690e2e6ecfa9b89e9cd67d3d01b8807f13
blackwolfinc <47728533+blackwolfinc@users.noreply.github.com>
2019-03-13 11:59:40 +0700

    new

--- Admin/navbar_editor.php
+++ Admin/navbar_editor.php
@@ -1,8 +1,8 @@
 <nav class="navbar navbar-default navbar-fixed-top">
     <div class="container-fluid">
         <div class="navbar-header">
-            <a class="navbar-brand" href="#">
-                Logo
+            <a class="navbar-brand" href="#" style="padding-top: 8px;">
+           		<img src="../../img/logoas.png" class="user-image img-responsive" style="width: 115px; height: 35px; padding-top: 0px; ">
             </a>
         </div>
     <?php include 'proses/back.php'; ?>
