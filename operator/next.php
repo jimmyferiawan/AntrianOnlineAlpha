10bab236aa5bed1dd080e66b3db685ed107edb1c
blackwolfinc <47728533+blackwolfinc@users.noreply.github.com>
2019-09-12 20:16:15 +0700

    new

--- operator/next.php
+++ operator/next.php
@@ -1,25 +1,5 @@
 <?php 
 
-/*$nexta = $now+1;
-$sql_op = mysqli_query($conn, "SELECT no_antrian, status_temp, pin_temp  FROM temp WHERE lokasi = '$lokasiberobat' AND no_antrian='$nexta'  ");
-
-
-	$sql_antri = mysqli_query($conn, "select  total, sekarang from antri where lokasi = '$lokasiberobat'  ");
-	$row = mysqli_fetch_array($sql_antri);
-	$total = $row[0];
-	$skrg = $row[1];
-	$_SESSION["loc"]["total"]=$total;
-
-	$list_op = mysqli_fetch_array($sql_op);
-	$nx_antri = $list_op[0];
-	$nx_status = $list_op[1];
-	$nx_pin = $list_op[2];
-	 $_SESSION["nx"]["no_antrian"] =  $nx_antri;
-	 $_SESSION["nx"]["status_temp"] = $nx_status;
-	 $_SESSION["nx"]["pin_temp"] = $nx_pin;
-	//$pin=( $_SESSION["nx"]["pin_temp"]);
-	//$qq= $_SESSION["nx"]["status_temp"] ;
-	//$tot=$_SESSION["loc"]["total"];*/
 	$sql_op = mysqli_query($conn, "select  total, sekarang from antri where lokasi = '$lokasiberobat'  ");
 	$col = mysqli_fetch_array($sql_op);
 	$total = $col[0];
@@ -33,19 +13,15 @@ $sql_op = mysqli_query($conn, "SELECT no_antrian, status_temp, pin_temp  FROM te
 	$sql_antri = mysqli_query($conn, "SELECT no_antrian, status_temp, pin_temp, siklus_temp  FROM temp WHERE lokasi = '$lokasiberobat' AND no_antrian>$now ORDER BY no_antrian ASC ");
 	$count = mysqli_num_rows($sql_antri);
 
-	/*$sql_siklus =mysqli_query($conn, "SELECT siklus_temp FROM temp WHERE lokasi= '$lokasiberobat' AND no_antrian='$now' ");
-	$siklus = mysqli_fetch_array($sql_siklus);
-	$siklus_temp = $siklus[0];
-	*/
 	if($count_stat>0){
 	if($count>0){
 		while ($row = mysqli_fetch_assoc($sql_antri)){
 			if($row['status_temp']==2){
-
 				if($total-$now>=$loop){
 					$now++;
 					if($row['siklus_temp']<2){
-						$u = mysqli_query($conn, "UPDATE temp SET siklus_temp=2 WHERE pin_temp= '".$row['pin_temp']."' ");
+						$siklus = $row['siklus_temp']+1;
+						$u = mysqli_query($conn, "UPDATE temp SET siklus_temp='$siklus' WHERE pin_temp= '".$row['pin_temp']."' ");
 						$total++;
 						$s = mysqli_query($conn, "UPDATE temp SET no_antrian  = $total where pin_temp  =  '".$row['pin_temp']."'");
 					}else{
@@ -64,5 +40,5 @@ $sql_op = mysqli_query($conn, "SELECT no_antrian, status_temp, pin_temp  FROM te
 		$s = mysqli_query($conn, "UPDATE antri SET sekarang=$now , total = $total where lokasi =  '$lokasiberobat'");
 	}
 	}
-	 //header("refresh: 0;");
+	 
  ?>
