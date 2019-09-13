a40e4dff9092aebbeeb788c761e827d003b7e610
bagas <bagasajin@gmail.com>
2019-09-13 08:22:04 +0700

    dcfds

---
+++ bpjs.php
@@ -0,0 +1,24 @@
+<?php 
+session_start();
+ ?>
+<!DOCTYPE html>
+
+<html>
+<head>
+	<title> tes </title>
+</head>
+<body>
+		<div class="btn bnt-sm" name="adminscr" id="adminscr">
+			<button>
+						<a href="/AntrianOnlineAlpha/login_AD.php" >BPJS</a>
+						</button>
+					</div>
+					<button>
+							<div class="btn bnt-sm" name="adminscr" id="adminscr">
+						<a href="/AntrianOnlineAlpha/login_AD.php"  >Umum</a>
+					</div>
+					</button>
+
+</div>
+</body>
+</html>
---
+++ user/bpjs.php
@@ -0,0 +1,20 @@
+<?php 
+session_start();
+ ?>
+<!DOCTYPE html>
+
+<html>
+<head>
+	<title> tes </title>
+</head>
+<body>
+		<div class="btn bnt-sm" name="adminscr" id="adminscr">
+						<a href="/AntrianOnlineAlpha/login_AD.php" >BPJS</a>
+					</div>
+							<div class="btn bnt-sm" name="adminscr" id="adminscr">
+						<a href="/AntrianOnlineAlpha/login_AD.php"  >Umum</a>
+					</div>
+<button></button>
+</div>
+</body>
+</html>
